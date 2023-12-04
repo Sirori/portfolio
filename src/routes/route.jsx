ebd316867f7b6480b7d197975c6d6416c1ca01a5
Sirori <tldus7519@gmail.com>
2023-12-04 13:29:36 +0900

    ✨ [feat] 초기 세팅
    라우트 설정, 폰트 설정 등
    
    fixes: 추후 변경 가능성 O

---
+++ src/routes/route.jsx
@@ -0,0 +1,27 @@
+import { lazy } from "react";
+import {
+	Route,
+	createHashRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+const RootLayout = lazy(() => import("./../layout/RootLayout"));
+const Home = lazy(() => import("../pages/Home"));
+const About = lazy(() => import("../pages/About"));
+const Works = lazy(() => import("../pages/Works"));
+
+const router = createHashRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route
+				index
+				element={
+						<Home />
+				}
+			/>
+			<Route path="/about" element={<About />} />
+			<Route path="/works" element={<Works />} />
+		</Route>
+	)
+);
+
+export default router;
