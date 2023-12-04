cd3633ce983d8f9e7755cba0fd553b0ffd4705e4
Sirori <tldus7519@gmail.com>
2023-12-04 16:12:50 +0900

    ✨ [feat] Spinner 추가
    스피너 추가
    
    fixes: 디자인과 필요한 부분은 수정 예정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,9 @@ function App() {
 			<HelmetProvider>
 				<QueryClientProvider client={queryClient}>
 					<div className="App">
+						<Suspense fallback="..loading">
 							<RouterProvider router={router} />
+						</Suspense>
 					</div>
 					<ReactQueryDevtools />
 				</QueryClientProvider>
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -25,3 +25,4 @@ const router = createHashRouter(
 );
 
 export default router;
+
