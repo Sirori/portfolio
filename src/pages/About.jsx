c8e518831a3d304b3f14fb6fef6c5889c3e1acba
Sirori <tldus7519@gmail.com>
2024-02-07 20:14:11 +0900

    🚀 [deploy] meta 태그 추가
    SEO 개선을 위한 meta 태그 추가
    
    fixes: 링크 수정 예정

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
-import Spinner from "./../components/Spinner";
+import { Helmet } from "react-helmet-async";
 import AboutMain from "../components/about/AboutMain";
 import Education from "../components/about/Education";
 import Skills from '../components/about/Skills';
 
 
 function About(){
-  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
+      <Helmet>
+				<title>양시연 포트폴리오 works</title>
+				<meta property="title" content="포트폴리오 about 페이지" />
+				<meta
+					name="description"
+					content="양시연 2024 프론트엔드/웹 퍼블리셔 포트폴리오"
+				/>
+				<meta property="og:title" content="양시연 2024 포트폴리오" />
+				<meta property="og:description" content="양시연 2024 프론트엔드/웹 퍼블리셔 포트폴리오" />
+				<meta
+					property="og:url"
+					content=""
+				/>
+			</Helmet>
       <AboutMain />
       <Education />
       <Skills />
