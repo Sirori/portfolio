347df486e8412d75efc66823e49428d58bc0d787
Sirori <tldus7519@gmail.com>
2024-02-07 16:07:23 +0900

    🐛 [fix] 스크롤 문제 해결
    x축 스크롤이 안되는 문제 해결
    
    fixes: 추후 수정 가능성 O

--- src/components/home/HorizonContents.jsx
+++ src/components/home/HorizonContents.jsx
@@ -1,84 +1,81 @@
-import { useRef, useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
-import S from "./Horizon.module.css";
-import pb from "./../../api/pocketbase";
-import { getPbImageURL } from "./../../hooks/getPbImageURL";
-
-import gimpo from "./../../assets/image/gimpoMain.webp"
-import jinheung from "./../../assets/image/jinheungMain.webp"
-import karly from "./../../assets/image/karlyMain.webp"
-import netspa from "./../../assets/image/netspaMain.webp"
-import taing from "./../../assets/image/taingMain.webp"
+import { useRef, useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import S from './Horizon.module.css';
+import pb from './../../api/pocketbase';
+import { getPbImageURL } from './../../hooks/getPbImageURL';
 
 function HorizonContents() {
-  // 스크롤 이벤트
   const scrollContainerRef = useRef(null);
 
   useEffect(() => {
-  const scrollContainer = scrollContainerRef.current;
+    const scrollContainer = scrollContainerRef.current;
 
-  const handleWheel = (evt) => {
-    let containerStart = scrollContainer.scrollLeft === 0;
-    let containerEnd = scrollContainer.scrollLeft + scrollContainer.offsetWidth >= scrollContainer.scrollWidth;
-    let containerFinished = scrollContainer.scrollLeft + scrollContainer.offsetWidth <= scrollContainer.scrollWidth;
-    let scrollingUp = (scrollContainer.deltaY < 0);
-    let scrollingDown = (scrollContainer.deltaY > 0);
-    if((containerStart && scrollingDown) || (containerEnd) || (containerEnd.scrollingUp)){
-      console.log(scrollContainer.scrollLeft)
-      return;
-    }else{
-      evt.preventDefault();
-      scrollContainer.scrollLeft += evt.deltaY;
-      console.log(scrollContainer.scrollLeft)
-    }
-  };
+    const handleWheel = (evt) => {
+      const { deltaY } = evt;
+      const { scrollLeft, offsetWidth, scrollWidth } = scrollContainer;
 
-  if (scrollContainer) {
-    scrollContainer.addEventListener("wheel", handleWheel);
-  }
+      const OFFSET = 10;
+      const isStart = scrollLeft === 0;
+      const moveLeft = scrollLeft + offsetWidth;
+      const isEnd = moveLeft >= scrollWidth - OFFSET;
+      const isContinue = !isEnd && !isStart;
 
-  // return () => {
-  //     if (scrollContainer) {
-  //       scrollContainer.removeEventListener("wheel", handleWheel);
-  //     }
-  //   };
+      const isScrollUp = deltaY < 0;
+      const isScrollDown = deltaY > 0;
+
+      if (isScrollDown || isScrollUp) {
+        scrollContainer.scrollLeft += deltaY;
+      }
+
+      if (isContinue) {
+        evt.preventDefault();
+      }
+    };
+
+    scrollContainer?.addEventListener('wheel', handleWheel);
+
+    return () => {
+      scrollContainer?.removeEventListener('wheel', handleWheel);
+    };
   }, []);
-  
+
   // pb데이터 가져오기
   const [contents, setContents] = useState([]);
-	const [status, setStatus] = useState("pending");
-	const [error, setError] = useState(null);
+  const [status, setStatus] = useState('pending');
+  const [error, setError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-		setStatus("loading");
+    setStatus('loading');
 
-		Promise.all([
-			pb.collection("project").getFullList(),
-		])
-			.then(([project]) => {
-				setContents([
-					{ title: "프로젝트", data: project }
-				]);
-				setStatus("success");
-			})
-			.catch((error) => {
-				setError(error);
-				setStatus("error");
-			});
-	}, [id]);
-  
+    Promise.all([pb.collection('project').getFullList()])
+      .then(([project]) => {
+        setContents([{ title: '프로젝트', data: project }]);
+        setStatus('success');
+      })
+      .catch((error) => {
+        setError(error);
+        setStatus('error');
+      });
+  }, [id]);
+
   return (
     <section className={S.horizontal}>
       <div className={S.verticalScrollContainer}>
         <div className={S.scrollContainer} ref={scrollContainerRef}>
-          {contents?.map((contentCategory)=> contentCategory.data?.map((item)=>(
-            <div className={S.horizontalContent} key={item.id}>
-              <Link to={`/detail/${item.id}`} className={S.contentItem}>
-                <img className="w-full h-auto" src={getPbImageURL(item, "mainImage")} alt={`${item.title} 클론코딩`} />
-              </Link>
-            </div>
-          )))}
+          {contents?.map((contentCategory) =>
+            contentCategory.data?.map((item) => (
+              <div className={S.horizontalContent} key={item.id}>
+                <Link to={`/detail/${item.id}`} className={S.contentItem}>
+                  <img
+                    className="w-full h-auto"
+                    src={getPbImageURL(item, 'mainImage')}
+                    alt={`${item.title} 클론코딩`}
+                  />
+                </Link>
+              </div>
+            ))
+          )}
         </div>
       </div>
     </section>
