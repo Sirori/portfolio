5f0ae534eba9cbcd42b93e0d7d8e501ae4d662e0
Sirori <tldus7519@gmail.com>
2024-02-07 20:11:54 +0900

    ✨ [feat] pending 페이지 추가
    로딩 중 페이지 추가

---
+++ src/components/common/PendingPage.jsx
@@ -0,0 +1,11 @@
+function PendingPage() {
+	return (
+		<div className="bg-white h-[100vh] py-[30%] w-full items-center flex flex-col">
+			<p className="text-gray-300 text-3xl">
+        잠시만 기다려주세요...
+      </p>
+		</div>
+	);
+}
+
+export default PendingPage;
