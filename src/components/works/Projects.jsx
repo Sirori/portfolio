9accb2cc4c9deb187fd2eaaba94a1270cff7bacd
Sirori <tldus7519@gmail.com>
2024-02-14 21:54:49 +0900

    🐛 [fix] 레이아웃 수정(질문을 위한 임시 커밋)
    레이아웃 전면 수정
    
    fixes: 오류 수정 중

--- src/components/works/Projects.jsx
+++ src/components/works/Projects.jsx
@@ -1,26 +1,21 @@
 import S from "./Works.module.css"
+import React from "react";
 import { useRef, useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, EffectCoverflow } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 import pb from "./../../api/pocketbase";
 import { getPbImageURL } from "./../../hooks/getPbImageURL";
+import FeatureBlock from "./../common/FeatureBlock";
 
 function Projects() {
-  const swiperRef = useRef(null);
   const [contents, setContents] = useState([]);
 	const [status, setStatus] = useState("pending");
 	const [error, setError] = useState(null);
   const { id } = useParams();
-
-  useEffect(() => {
-    if (swiperRef.current) {
-      swiperRef.current.navigation.update();
-    }
-  }, []);
 
 	useEffect(() => {
 		setStatus("loading");
@@ -41,37 +36,48 @@ function Projects() {
 	}, [id]);
 
   return (
-    <section className={S.projects}>
+    <section className={`projects ${S.projects}`}>
       <h2 className={S.worksTitle}>Works</h2>
-      <div className={S.projectContainer}>
+      <div className={`projectContainer ${S.projectContainer}`}>
         <h3 className={S.projectTitle}>Projects</h3>
         <Swiper 
-        className={S.swiper}
-        slidesPerView={3}
-        loop={true}
-        centeredSlides={true}
-        effect={"coverflow"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 200,
-          depth: 0,
-          modifier: 5,
-          slideShadows : false,
-        }}
-        slideToClickedSlide={false}
+        className={`swiper-container ${S.swiper}`}
+        pagination={{ clickable: true }}
         navigation={{
-          prevEl: ".swiper-button-next",
-          nextEl: ".swiper-button-prev",
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
         }}
-        modules={[Navigation, EffectCoverflow]}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        loop={true}
+        modules={[Navigation, Pagination]}
         >
           {contents?.map((contentCategory)=>
             contentCategory.data?.map((item)=>(
               <>
-                <SwiperSlide key={item.id} className={S.swiperItem}>
-                  <Link to={`/detail/${item.id}`} className={`swiperLink ${S.swiperLink}`}>
-                    <img className={S.swiperImg} src={getPbImageURL(item, "mainImage")} alt={`${item.title} 클론코딩`} />
+                <SwiperSlide key={item.id} className={`projectItem ${S.projectItem}`}>
+                  <Link to={`/detail/${item.id}`} className={`projectLink ${S.projectLink}`}>
+                    <div className={S.projectImg}>
+                      <img className={S.swiperImg} src={getPbImageURL(item, "mainImage")} alt={`${item.title} 클론코딩`} />
+                    </div>
+                    <div className={S.txtContainer}>
+                      <h4 className={S.itemTitle}>{item.title}</h4>
+                      <div className={S.blockBox}>
+                      <FeatureBlock content={item.block.split(", ")[0]} />
+                      <FeatureBlock content={item.block.split(", ")[1]} />
+                      <FeatureBlock content={item.block.split(", ")[2]} />
+                      </div>
+                      <p className={S.itemDesc}>
+                        {item.simpleDesc.includes("br") ? (
+                          item.simpleDesc.split("br").map((desc, index) => (
+                            <React.Fragment key={index}>
+                              {desc}
+                              <br />
+                            </React.Fragment>
+                          ))
+                        ) : (
+                          item.simpleDesc
+                        )}
+                      </p>
+                    </div>
                   </Link>
                 </SwiperSlide>
               </>
