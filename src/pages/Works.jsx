68977ea81bb9f5003e59520506cdafcf25d4cc22
Sirori <tldus7519@gmail.com>
2024-02-20 15:45:12 +0900

    불필요한부분 제거

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,11 +1,8 @@
 import { Helmet } from "react-helmet-async";
-import Spinner from "./../components/Spinner";
-import { useState } from "react";
-import Projects from '../components/works/Projects';
-import Photoshop from '../components/works/Photoshop';
+import Projects from "../components/works/Projects";
+import Photoshop from "../components/works/Photoshop";
 
 function Works(){
-  const [isLoading, setIsLoading] = useState(true);
   return (
     <>
       <Helmet>
